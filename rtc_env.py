ef5e6b6e2ada27d4f07075eb79458b3f4246bcbf
thegreatwb <wangbo199881@hotmail.com>
2021-05-17 16:56:06 +0800

    add config

--- rtc_env.py
+++ rtc_env.py
@@ -6,7 +6,7 @@ import os
 import random
 import numpy as np
 import glob
-
+from Queue import Queue
 import gym
 from gym import spaces
 
@@ -19,6 +19,8 @@ from alphartc_gym.utils.packet_record import PacketRecord
 UNIT_M = 1000000
 MAX_BANDWIDTH_MBPS = 8
 MIN_BANDWIDTH_MBPS = 0.01
+HISTORY_LENGTH = 10
+STATE_DIMENSION = 4
 LOG_MAX_BANDWIDTH_MBPS = np.log(MAX_BANDWIDTH_MBPS)
 LOG_MIN_BANDWIDTH_MBPS = np.log(MIN_BANDWIDTH_MBPS)
 
@@ -45,18 +47,31 @@ class GymEnv:
         self.trace_set = glob.glob(f'{trace_dir}/**/*.json', recursive=True)
         self.action_space = spaces.Box(low=0.0, high=1.0, shape=(1,), dtype=np.float64)
         self.observation_space = spaces.Box(
-            low=np.array([0.0, 0.0, 0.0, 0.0]),
-            high=np.array([1.0, 1.0, 1.0, 1.0]),
+            low=np.zeros((STATE_DIMENSION,HISTORY_LENGTH)),
+            high=np.ones((STATE_DIMENSION,HISTORY_LENGTH)),
             dtype=np.float64)
 
-    def reset(self):
+
+    def reset(self):#reset history of state and return np.array
         self.gym_env = alphartc_gym.Gym()
         self.gym_env.reset(trace_path=random.choice(self.trace_set),
             report_interval_ms=self.step_time,
             duration_time_ms=0)
         self.packet_record = PacketRecord()
         self.packet_record.reset()
-        return [0.0, 0.0, 0.0, 0.0]
+        self.receiving_rate=np.zeros(HISTORY_LENGTH)
+        self.delay=np.zeros(HISTORY_LENGTH)
+        self.loss_ratio=np.zeros(HISTORY_LENGTH)
+        self.prediction_history=np.zeros(HISTORY_LENGTH)
+
+        states=np.vstack((self.receiving_rate,self.delay,self.loss_ratio,self.prediction_history))
+
+        return states
+
+    def get_reward(self):
+        reward = self.receiving_rate[HISTORY_LENGTH-1] - self.delay[HISTORY_LENGTH-1] - self.loss_ratio[HISTORY_LENGTH-1]
+
+        return reward
 
     def step(self, action):
         # action: log to linear
@@ -78,17 +93,25 @@ class GymEnv:
             self.packet_record.on_receive(packet_info)
 
         # calculate state
-        states = []
-        receiving_rate = self.packet_record.calculate_receiving_rate(interval=self.step_time)
-        states.append(liner_to_log(receiving_rate))
-        delay = self.packet_record.calculate_average_delay(interval=self.step_time)
-        states.append(min(delay/1000, 1))
-        loss_ratio = self.packet_record.calculate_loss_ratio(interval=self.step_time)
-        states.append(loss_ratio)
-        latest_prediction = self.packet_record.calculate_latest_prediction()
-        states.append(liner_to_log(latest_prediction))
 
+        receiving_rate = self.packet_record.calculate_receiving_rate(interval=self.step_time)
+        #states.append(liner_to_log(receiving_rate))
+        self.receiving_rate.append(receiving_rate)
+        np.delete(self.receiving_rate, 0, axis=0)
+        delay = self.packet_record.calculate_average_delay(interval=self.step_time)
+        #states.append(min(delay/1000, 1))
+        self.delay.append(delay )
+        np.delete(self.delay, 0, axis=0)
+        loss_ratio = self.packet_record.calculate_loss_ratio(interval=self.step_time)
+        #states.append(loss_ratio)
+        self.loss_ratio.append(loss_ratio)
+        np.delete(self.loss_ratio, 0, axis=0)
+        latest_prediction = self.packet_record.calculate_latest_prediction()
+        #states.append(liner_to_log(latest_prediction))
+        self.prediction_history.append(latest_prediction)
+        np.delete(self.prediction_history, 0, axis=0)
+        states = np.vstack((self.receiving_rate, self.delay, self.loss_ratio, self.prediction_history))
         # calculate reward
-        reward = states[0] - states[1] - states[2]
+        reward = self.get_reward()
 
         return states, reward, done, {}
