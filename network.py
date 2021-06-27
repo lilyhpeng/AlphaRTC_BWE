098f7072a33fe79b79e23b07ee148f43bde65ddb
liiviiu <peng_h30@163.com>
2021-06-27 16:32:11 +0800

    feat:lab file sync

--- network.py
+++ network.py
@@ -78,6 +78,66 @@ class CriticNetwork(nn.Module):
         self.numFcInput = 3072
 
         self.discount = config['discount_factor']
+    # def create_network(self):
+    #     # Structure of Network
+    #     # self.net = tf.keras.models.Sequential()
+    #     # self.net.add(tf.keras.Input(shape=(self.s_dim, self.s_info)))
+    #     # self.net.add(tf.keras.layers.Dense(self.layer1_shape, activation='relu'))
+    #     # self.net.add(tf.keras.layers.Dense(self.layer2_shape, activation='relu'))
+    #     # self.net.add(tf.keras.layers.Dense(self.a_dim))
+    #     # inputs = tf.random.normal((self.s_dim, self.s_info))
+    #     #
+    #     #
+    #     # h1 = tf.keras.layers.Dense(units=self.layer1_shape, name='fc1')
+    #     # h1 = h1(inputs)
+    #     # h1 = tf.keras.layers.BatchNormalization(h1, training=is_training, scale=False)
+    #     # h1 = tf.nn.relu(h1)fs
+    #     #
+    #     # h2 = tf.keras.layers.Dense(units=self.layer2_shape, name='fc2')
+    #     # h2 = h2(h1)
+    #     # h2 = tf.keras.layers.BatchNormalization(h2, training=is_training, scale=False)
+    #     # h2 = tf.nn.relu(h2)
+    #
+    #     # output = tf.keras.layers.Dense(units=self.a_dim, activation=tf.nn.tanh)
+    #     # inputs = tflearn.input_data(shape=[self.s_dim, self.s_info])
+    #     #
+    #     # # Network Structure (Fully Connected)
+    #     # h1 = tflearn.fully_connected(inputs, self.layer1_shape, activation='leaky_relu')
+    #     # h2 = tflearn.fully_connected(h1, self.layer2_shape, activation='leaky_relu')
+    #     # output = tflearn.fully_connected(h2, self.a_dim, activation='linear')
+    #
+    #     # (Pytorch) Network Structure
+    #     self.net = nn.Sequential(
+    #         nn.Linear(self.s_dim * self.s_info, self.layer1_shape),
+    #         nn.LeakyReLU(),
+    #         nn.Linear(self.layer1_shape, self.layer2_shape),
+    #         nn.LeakyReLU(),
+    #         nn.Linear(self.layer2_shape, self.a_dim),
+    #         nn.Tanh()
+    #     )
+    #
+    #     # Network Structure (Convolutional)
+    #     # h1 = tflearn.conv_1d(inputs, self.layer1_shape, 4, activation='relu')
+    #     # h2 = tflearn.fully_connected(h1, self.layer2_shape, activation='relu')
+    #     # output = tflearn.fully_connected(h2, self.a_dim, activation='linear')
+    #
+    #     # inputs = tflearn.input_data(shape=[None, self.s_dim[0], self.s_dim[1]])
+    #     # split_0 = tflearn.fully_connected(inputs[:, 0:1, -1], 128, activation='relu')
+    #     # split_1 = tflearn.fully_connected(inputs[:, 1:2, -1], 128, activation='relu')
+    #     # split_2 = tflearn.conv_1d(inputs[:, 2:3, :], 128, 4, activation='relu')
+    #     # split_3 = tflearn.conv_1d(inputs[:, 3:4, :], 128, 4, activation='relu')
+    #     # split_4 = tflearn.conv_1d(inputs[:, 4:5, :A_DIM], 128, 4, activation='relu')
+    #     # split_5 = tflearn.fully_connected(inputs[:, 4:5, -1], 128, activation='relu')
+    #     #
+    #     # split_2_flat = tflearn.flatten(split_2)
+    #     # split_3_flat = tflearn.flatten(split_3)
+    #     # split_4_flat = tflearn.flatten(split_4)
+    #     #
+    #     # merge_net = tflearn.merge([split_0, split_1, split_2_flat, split_3_flat, split_4_flat, split_5], 'concat')
+    #     #
+    #     # dense_net_0 = tflearn.fully_connected(merge_net, 128, activation='relu')
+    #     # out = tflearn.fully_connected(dense_net_0, self.a_dim, activation='softmax')
+    #     # return inputs, output
 
         self.rConv1d = nn.Conv1d(1, self.layer1_shape, 3)
         self.dConv1d = nn.Conv1d(1, self.layer1_shape, 3)
